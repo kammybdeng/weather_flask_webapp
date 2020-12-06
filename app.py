fdb5489ef3879d1239943a0f7035d1ebe18e64cb
Kammy Deng <=>
2020-12-06 15:33:29 -0800

    added css styles and input error exceptions

--- app.py
+++ app.py
@@ -18,13 +18,19 @@ def check_weather():
 	city = request.args.get('cityname')
 	state= request.args.get('statename')
 	country = request.args.get('countryname')
-	#print('print this', type(state), type(country))
+	if city == '':
+		message = 'Please provide a city name'
+		error_message = {'city': city.title(),
+						 'message': message}
+		return render_template('error.html', error_message=error_message)
 	if state == '' and country.lower()=='us':
 		state = default_state
 	if country == '':
 		country = default_country
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={},{},{}&appid={}'.format(city, state, country, API_KEY)
-	response = requests.get(url).json()
+	#response = requests.get(url).json()
+	response = {'coord': {'lon': -122.42, 'lat': 37.77}, 'weather': [{'id': 801, 'main': 'Clouds', 'description': 'few clouds', 'icon': '02n'}], 'base': 'stations', 'main': {'temp': 282.7, 'feels_like': 277.89, 'temp_min': 281.48, 'temp_max': 284.15, 'pressure': 1026, 'humidity': 81}, 'visibility': 10000, 'wind': {'speed': 5.7, 'deg': 300}, 'clouds': {'all': 20}, 'dt': 1607229456, 'sys': {'type': 1, 'id': 5817, 'country': 'US', 'sunrise': 1607181044, 'sunset': 1607215848}, 'timezone': -28800, 'id': 5391959, 'name': 'San Francisco', 'cod': 200}
+	print(response)
 	if response.get('cod') != 200:
 		message = response.get('message', '')
 		error_message = {'city': city.title(),
