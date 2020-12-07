e5ab4d876883f34d6efaf634e555b98d306fb433
Kammy Deng <=>
2020-12-06 22:27:41 -0800

    updated styling and added 5 days of forecast

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from decouple import config
 import requests
-from helper import kelvin_to_celsius, kelvin_to_fahrenheit
+from helper import kelvin_to_celsius, kelvin_to_fahrenheit, forecast_api_request
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -13,6 +13,7 @@ def index():
 
 @app.route('/check_weather', methods=['GET'])
 def check_weather():
+	weather_dict = dict()
 	default_state = 'CA'
 	default_country = 'US'
 	city = request.args.get('cityname')
@@ -29,22 +30,15 @@ def check_weather():
 		country = default_country
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={},{},{}&appid={}'.format(city, state, country, API_KEY)
 	response = requests.get(url).json()
+	print()
 	if response.get('cod') != 200:
 		message = response.get('message', '')
 		error_message = {'city': city.title(),
 						 'message': message}
 		return render_template('error.html', error_message=error_message)
 	else:
-		weather = {
-		'city' : response['name'].title(),
-		'state': state.upper(),
-		'country': response['sys']['country'].upper(),
-		'celsius': kelvin_to_celsius(response['main']['feels_like']),
-		'fahrenheit': kelvin_to_fahrenheit(response['main']['feels_like']),
-		'description': response['weather'][0]['description']
-		}
-
-	return render_template('weather.html', weather=weather)
+		weather_dict = forecast_api_request(response, API_KEY)
+	return render_template('weather.html', weather=weather_dict)
 
 if __name__ == '__main__':
     app.run()
