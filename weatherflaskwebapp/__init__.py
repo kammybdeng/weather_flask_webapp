1104019bc240743c2487f70a07cbce7e9865396e
kammybdeng <kbdeng18@gmail.com>
2021-06-21 23:12:51 -0700

    fix config file for deployment

--- weatherflaskwebapp/__init__.py
+++ weatherflaskwebapp/__init__.py
@@ -1,18 +1,19 @@
+import os
 from decouple import config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
-app.config['FLASK_DEBUG'] = config('FLASK_DEBUG')
-app.config['SECRET_KEY'] = config('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+app.config.from_object(os.getenv('APP_SETTINGS', "config.ProductionConfig"))
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-
 
 from weatherflaskwebapp import routes
